5a0b583032a8f11489479c1c1e267ca4d9389080
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-22 07:38:59 -0700

    Task 14 - Advanced - iterateThroughObject: param = reportWithIterator >> createIteratorObject(value) - correction

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,13 +2,13 @@ export default function createReportObject(employeesList) {
   const allEmployees = {};
 
   for (const [departmentName, employees] of Object.entries(employeesList)) {
-    allEmployees[departmentName] = [...employees];
+    allEmployees[departmentName] = employees.join(' | ');
   }
 
   return {
     allEmployees,
     getNumberOfDepartments() {
-      return Object.keys(employeesList.join(' | ')).length;
+      return Object.keys(employeesList.join).length;
     },
   };
 }
