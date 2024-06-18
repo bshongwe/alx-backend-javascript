b49d65e09136457486747c6f1020f21ba65224a2
bshongwe <dru.da.beat.hustle@gmail.com>
2024-06-18 09:12:02 +0200

    Task 0: ✨ Add 0-console.js to display a message on STDOUT 🖥️

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Task 0 Displays message on STDOUT.
+ * @param {String} msg message printed using Console Log.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
