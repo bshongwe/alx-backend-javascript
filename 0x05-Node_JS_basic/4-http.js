23fd5483ca6417b5d2496407fb4eff9820122ebd
bshongwe <dru.da.beat.hustle@gmail.com>
2024-06-18 15:03:29 +0200

    Task 3 tests, Task 4: 🌐 Implement HTTP server to respond with a welcome message 📡

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,22 @@
+const app = http.createServer();
+
+/**
+ * Handles incoming HTTP requests and sends a response with a welcome message.
+ */
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
+
+/**
+ * Starts the HTTP server and listens on the specified host and port.
+ */
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
